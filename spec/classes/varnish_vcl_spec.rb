9a817ca6b5f3cd77011cef4f06f130994de1f21b
Lukas Plattner <lukas.plattner@apa.at>
2022-03-07 11:33:12 +0100

    Add some Tests

--- spec/classes/varnish_vcl_spec.rb
+++ spec/classes/varnish_vcl_spec.rb
@@ -32,10 +32,32 @@ describe 'varnish::vcl', type: :class do
     it { is_expected.to contain_varnish__vcl__includefile('acls') }
     it { is_expected.to contain_varnish__vcl__includefile('backendselection') }
     it { is_expected.to contain_varnish__vcl__includefile('waf') }
+    it { is_expected.to contain_concat__fragment('waf').with_target('/etc/varnish/includes/waf.vcl') }
+
+    #Contents of Includefiles
+    it { is_expected.to contain_concat('/etc/varnish/includes/probes.vcl') }
+    it { is_expected.to contain_concat('/etc/varnish/includes/backends.vcl') }
+    it { is_expected.to contain_concat('/etc/varnish/includes/directors.vcl') }
+    it { is_expected.to contain_concat('/etc/varnish/includes/acls.vcl') }
+    it { is_expected.to contain_concat('/etc/varnish/includes/backendselection.vcl') }
+    it { is_expected.to contain_concat('/etc/varnish/includes/waf.vcl') }
+
+    it { is_expected.to contain_concat__fragment('probes-header').with_target('/etc/varnish/includes/probes.vcl') }
+    it { is_expected.to contain_concat__fragment('backends-header').with_target('/etc/varnish/includes/backends.vcl') }
+    it { is_expected.to contain_concat__fragment('directors-header').with_target('/etc/varnish/includes/directors.vcl') }
+    it { is_expected.to contain_concat__fragment('acls-header').with_target('/etc/varnish/includes/acls.vcl') }
+    it { is_expected.to contain_concat__fragment('backendselection-header').with_target('/etc/varnish/includes/backendselection.vcl') }
+    it { is_expected.to contain_concat__fragment('waf-header').with_target('/etc/varnish/includes/waf.vcl') }
+
 
     # Backends
     it { is_expected.to contain_varnish__backend('default').with_host('127.0.0.1') }
     it { is_expected.to contain_varnish__backend('default').with_port('8080') }
+
+    #Default ACL
+    it { is_expected.to contain_varnish__acl('blockedips').with_hosts([]) }
+    it { is_expected.to contain_varnish__acl('unset_headers_debugips').with_hosts([ '172.0.0.1' ]) }
+    it { is_expected.to contain_varnish__acl('purge').with_hosts([]) }
   end
 
   context 'manual backends' do
