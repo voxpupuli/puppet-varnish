efb1a6cfba42022d57a73974aab245f8a1824143
Lukas Plattner <lukas.plattner@apa.at>
2023-07-02 21:30:11 +0200

    Add Pid param

--- manifests/ncsa.pp
+++ manifests/ncsa.pp
@@ -9,7 +9,7 @@
 class varnish::ncsa (
   Boolean $enable = true,
   Stdlib::Ensure::Service $service_ensure = 'running',
-  String $daemon_opts = '-a -w /var/log/varnish/varnishncsa.log -D',
+  String $daemon_opts = '-a -w /var/log/varnish/varnishncsa.log -D -P /run/varnishncsa/varnishncsa.pid',
 ) {
   systemd::dropin_file { 'varnishncsa_service':
     unit     => 'varnishncsa.service',
